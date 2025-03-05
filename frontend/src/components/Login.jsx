98189a08f44a4dff09dc9e39a585ca5908523080
Anushka Chouhan <anushkachouhan064@gmail.com>
2025-03-05 15:43:43 +0530

    Commit all changes

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -14,7 +14,7 @@ function Login() {
   const onSubmit = async (data) => {
     const userInfo = { email: data.email, password: data.password };
 
-        await axios.post("http://localhost:4001/user/login", userInfo).then((res) =>{
+        await axios.post("/user/login", userInfo).then((res) =>{
           console.log(res.data);
           if(res.data) { 
             toast.success('Login successfull!'); 
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -19,7 +19,7 @@ function Signup() {
        
         const userInfo = { fullname: data.fullname, email: data.email, password: data.password };
 
-        await axios.post("http://localhost:4001/user/signup", userInfo).then((res) =>{
+        await axios.post("/user/signup", userInfo).then((res) =>{
           console.log(res.data);
           if(res.data) { 
             toast.success('SignUp successfull!');
